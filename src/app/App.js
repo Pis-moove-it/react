56108f7370876f0a5a42cd73a0c1a8435adf2db8
nico-rodriguez <marconicolasrodriguez@gmail.com>
2018-09-30 22:41:09 -0300

    Added footer
    It's rendered at the bottom. It has copyright symbol and current year.

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import FooterComponent from './common/FooterComponent';
 
-const App = () => (<div />);
+const today = new Date();
+
+const App = () => (
+  <div>
+    <FooterComponent>
+      {'\u00A9'}
+      {'PIS'}
+      {' '}
+      {today.getFullYear()}
+    </FooterComponent>
+  </div>);
 
 export default hot(module)(App);
