d9a0acf78bb5c450f777c29414607f50b8806035
alexisartus96 <alexisartus@gmail.com>
2018-10-01 18:02:40 -0300

    Merge with master

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import Header from "./home/components/Header";
 
-const App = () => (<div />);
+const App = () => (
+    <div>
+        <Header/>r
+    </div>
+);
 
 export default hot(module)(App);
