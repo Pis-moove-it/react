a6b0b263e7c3d6e115e687b5b0f6b597e3cfa16f
alexisartus96 <alexisartus@gmail.com>
2018-10-11 18:33:43 -0300

    test back-to-top

---
+++ src/app/home/components/BackToTop.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import back from '../css/back.css';
+
+const BackToTop = () => (
+    <a href="#top" className="color: white"><button id="myBtn">^</button></a>
+);
+
+export default BackToTop;
