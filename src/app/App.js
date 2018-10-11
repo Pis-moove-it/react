8926246c5536cc2c1806be9087fc151296c783ee
nico-rodriguez <marconicolasrodriguez@gmail.com>
2018-10-11 18:13:41 -0300

    Changed div to Wrapper

--- src/app/App.js
+++ src/app/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Footer from './home/components/Footer';
 import Header from './home/components/Header';
 import MapContainer from './home/components/MapContainer';
+import Wrapper from './home/components/Wrapper';
 import global from './home/css/global.css';
 
 const App = () => (
-  <div>
+  <Wrapper>
     <Header />
     <MapContainer />
     <Footer />
-  </div>
+  </Wrapper>
 );
 
 export default App;
