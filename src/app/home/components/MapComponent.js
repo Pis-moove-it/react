e8b35b12693c8a46661ffd58c608c2c07c3e5462
nico-rodriguez <marconicolasrodriguez@gmail.com>
2018-10-14 15:48:41 -0300

    Adjusted flex value

--- src/app/home/components/MapComponent.js
+++ src/app/home/components/MapComponent.js
@@ -5,7 +5,7 @@ const MapComponent = styled.div`
   -moz-box-sizing: border-box;
   box-sizing: border-box;
   z-index: 0;
-  flex: 1;
+  flex: 3;
   height: calc(100vh - 55px); /* all vertical height minus header */
 `;
 
