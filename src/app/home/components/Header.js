62320f6044cef1a5f4b760385d11d948251c7732
alexisartus96 <alexisartus@gmail.com>
2018-10-21 14:05:42 -0300

    add fixed property to header

--- src/app/home/components/Header.js
+++ src/app/home/components/Header.js
@@ -6,16 +6,55 @@ import {
 } from '../styles/header';
 
 
-const Header = () => (
-  <Box>
-    <ReciclandoLogo src={logo} />
-    <SubBox>
-      <HeaderTitle>¿Dónde reciclo?</HeaderTitle>
-      <HeaderTitle>¿Qué reciclo?</HeaderTitle>
-      <HeaderTitle>¿Sabías qué?</HeaderTitle>
-    </SubBox>
-    <LeafLogo src={leafLogo} />
-  </Box>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollingLock: false,
+      position: 'relative',
+      animation: 'none',
+    };
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    if (window.scrollY > 200) {
+      this.setState({
+        scrollingLock: true,
+        position: 'fixed',
+        animation: 'fadeStickyIn .6s ease-in-out forwards',
+      });
+    } else if (window.scrollY < 200) {
+      this.setState({
+        scrollingLock: false,
+        position: 'relative',
+        animation: 'none',
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Box position={this.state.position} transition={this.state.animation}>
+        <ReciclandoLogo src={logo} />
+        <SubBox>
+          <HeaderTitle>¿Dónde reciclo?</HeaderTitle>
+          <HeaderTitle>¿Qué reciclo?</HeaderTitle>
+          <HeaderTitle>¿Sabías qué?</HeaderTitle>
+        </SubBox>
+        <LeafLogo src={leafLogo} />
+      </Box>
+    );
+  }
+}
 
 export default Header;
