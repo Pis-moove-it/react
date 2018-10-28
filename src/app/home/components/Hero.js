35057b0ad8b7877d094222057ef1102ed53d9e76
alexisartus96 <alexisartus@gmail.com>
2018-10-27 21:01:11 -0300

    Final Hero Component

--- src/app/home/components/Hero.js
+++ src/app/home/components/Hero.js
@@ -1,28 +1,30 @@
 import React from 'react';
+import {
+  Events, animateScroll as scroll, scroller,
+} from 'react-scroll';
 import heroImage from '../assets/heroImage.jpg';
 import arrowDown from '../assets/arrow-up.png';
 import {
-  HeroImage, HeroTitle, TextBox, PrimaryBox, GoToMap, Br, HeroSubTitle, SubTextBox
+  HeroImage, HeroTitle, TextBox, PrimaryBox, GoToMap, Br, HeroSubTitle, SubTextBox, GoToMapLink,
 } from '../styles/hero';
 
 class Hero extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-
-    this.handleScroll = this.handleScroll.bind(this);
-  }
-
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+    Events.scrollEvent.register('begin', function () {
+      console.log('begin', arguments);
+    });
+
+    Events.scrollEvent.register('end', function () {
+      console.log('end', arguments);
+    });
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll() {
+  scrollTo() {
+    scroller.scrollTo('scroll-to-element', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    });
   }
 
   render() {
@@ -32,16 +34,18 @@ class Hero extends React.Component {
         <TextBox>
           <HeroTitle>
               Se parte del futuro de nuestro planeta.
-          <Br />
+            <Br />
                Dejanos guiarte en tu camino hacia el reciclaje.
-            </HeroTitle>
+          </HeroTitle>
         </TextBox>
-          <HeroSubTitle>
-              <SubTextBox>
-                  Presione para continuar
-              </SubTextBox>
-          </HeroSubTitle>
-        <GoToMap src={arrowDown}/>
+        <HeroSubTitle>
+          <SubTextBox>
+              Presione para continuar
+          </SubTextBox>
+        </HeroSubTitle>
+        <GoToMapLink onClick={() => scroll.scrollTo(540)}>
+          <GoToMap src={arrowDown} />
+        </GoToMapLink>
       </PrimaryBox>
     );
   }
--- src/app/home/styles/hero.js
+++ src/app/home/styles/hero.js
@@ -7,8 +7,9 @@ export const PrimaryBox = styled.div`
 `;
 
 export const HeroImage = styled.img`
-    max-height: 92vh;
-    height: auto;
+    object-fit: cover;
+    min-height: 90vh;
+    height: 92vh;
     width: 100%;
     top: 0;
     left: 0;
@@ -21,19 +22,26 @@ export const TextBox = styled.div`
     left: 50%;
     width: 50vw;
     transform: translate(-50%, -50%);
+    
+    @media (max-width: 767px) {
+        width: 75vw;
+    }
 `;
 
 export const HeroTitle = styled.h2`
     font-size: 30px;
     font-family: Candara;
     background: rgba(0, 0, 0, 0.6);
+    @media (max-width: 767px) {
+        font-size: 20px;
+    }
 `;
 
 export const SubTextBox = styled.div`
     position: absolute;
     top: 75%;
     left: 50%;
-    width: 30%;
+    width: 100%;
     transform: translate(-50%, -50%);
 `;
 
@@ -42,20 +50,50 @@ export const HeroSubTitle = styled.h2`
     font-family: Candara,serif;
     font-weight: bolder;
     text-shadow: 1px 1px #000;
+    
+    @media (max-width: 767px) {
+        font-size: 15px;
+    }
 `;
 
 export const GoToMap = styled.img`
-    position: absolute;
-    top: 80%;
-    left: 48%;
     transform: translate(-50%, -50%);
     width: 50px;
     height: 50px;
+    cursor: pointer;
     -webkit-transform: rotate(180deg);
     -moz-transform: rotate(180deg);
     -o-transform: rotate(180deg);
     -ms-transform: rotate(180deg);
     transform: rotate(180deg);
+    
+    
+`;
+
+export const GoToMapLink = styled.a`
+    text-decoration: none;
+    width: 50%;
+    position: absolute;
+    top: 80%;
+    left: 25%;
+    
+    $:hover {
+      text-decoration:none;
+    }
+    
+    -webkit-animation: mymove 1.5s infinite;
+    animation: mymove 1.5s infinite;
+    
+    
+    @-webkit-keyframes mymove {
+        from {top: 80%;}
+        to {top: 82%;}
+    }
+    
+    @keyframes mymove {
+        from {top: 80%;}
+        to {top: 82%;}
+    }
 `;
 
 export const Br = styled.br`
