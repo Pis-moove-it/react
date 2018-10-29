d6e552fc096399f68f6b111dbbfb9a3e9ebfe6b1
alexisartus96 <alexisartus@gmail.com>
2018-10-28 22:39:57 -0300

    Updates

---
+++ precache-manifest.7b5d905e0a467d4fc3e9bfa1e1a069af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38ebc74debf3391756eaec99fb52c8ce",
+    "url": "/reciclando-web/static/media/landing50.38ebc74d.png"
+  },
+  {
+    "revision": "1b05b4ae68fb66f745c87467b030a325",
+    "url": "/reciclando-web/static/media/heroImage.1b05b4ae.jpg"
+  },
+  {
+    "revision": "e2a8a9ea62bbd470ae2d137b630b0d8a",
+    "url": "/reciclando-web/static/media/carton.e2a8a9ea.png"
+  },
+  {
+    "revision": "657a0b0bb11111a490fc",
+    "url": "/reciclando-web/static/js/runtime~main.657a0b0b.js"
+  },
+  {
+    "revision": "7d13896fb622ddb3c15e",
+    "url": "/reciclando-web/static/js/main.7d13896f.chunk.js"
+  },
+  {
+    "revision": "486fc31dfbceeedc1303",
+    "url": "/reciclando-web/static/js/1.486fc31d.chunk.js"
+  },
+  {
+    "revision": "7d13896fb622ddb3c15e",
+    "url": "/reciclando-web/static/css/main.5ac5b71f.chunk.css"
+  },
+  {
+    "revision": "d10f869f8dbf51aeaf7f0f96fb6dbf8a",
+    "url": "/reciclando-web/index.html"
+  }
+];
--- src/app/map/styles/BoxTitle.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const BoxTitle = styled.div`
-  flex: 1;
-`;
-
-export default BoxTitle;
