a74be63ae758a97d1a1f4374326ff1600f2f617d
Cecilia Pirotto <maria.cecilia.pirotto@fing.edu.uy>
2018-10-30 15:42:14 -0300

    Show questions and options, loading animation while component is not rendered yet

--- src/app/trivia/components/Trivia.js
+++ src/app/trivia/components/Trivia.js
@@ -3,58 +3,79 @@ import {
   Box, Title, SubBox, Option, Question, Triangle, TitleBox, BoxOption,
 } from '../styles/trivia';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
 
 class Trivia extends Component {
 
   
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       questions: [],
-      correct: 0
+      correct: 0,
+      loading: true
     };
 
   }
 
   componentDidMount() {
+    this.getQuestions()
+  }
+
+  getQuestions() {
+    this.setState({loading : true})
     axios.get('http://34.213.11.120/questions')
       .then( ({data}) => {
         this.setState(
-          { questions : data }
+          { questions : data,
+          loading: false }
+
         );
-        console.log(this.state.questions.pop().question)
-        console.log(this.state.questions.pop().question)
-        //console.log(this.state.questions.pop().question)
+        console.log(this.state.questions.pop())
       })
       .catch((err) => {})
   }
 
 
 
+
 render(){
-return (
-  <Box>  
-    <TitleBox>
-      <Title>¿Cuánto conocés?</Title>
-      <Triangle />
-    </TitleBox>
-    <SubBox>
-      <Question></Question>
-      <BoxOption>
-        <Option>10 años</Option>
-      </BoxOption>    
-      <BoxOption>
-        <Option>500 años</Option>
-      </BoxOption>   
-      <BoxOption>
-        <Option>100 años</Option>
-      </BoxOption> 
-      <BoxOption>
-        <Option>700 años</Option>
-      </BoxOption> 
-    </SubBox>
-  </Box>
-);
+  const actualQ = this.state.questions.pop()
+  if(!this.state.loading ){ 
+    return (
+      <Box>  
+        <TitleBox>
+          <Title>¿Cuánto conocés?</Title>
+          <Triangle />
+        </TitleBox>
+        <SubBox>
+          <Question>{actualQ.question}</Question>
+          <BoxOption>
+            <Option>{actualQ.option_a}</Option>
+          </BoxOption>    
+          <BoxOption>
+            <Option>{actualQ.option_b}</Option>
+          </BoxOption>   
+          <BoxOption>
+            <Option>{actualQ.option_c}</Option>
+          </BoxOption> 
+          <BoxOption>
+            <Option>{actualQ.option_d}</Option>
+          </BoxOption> 
+        </SubBox>
+      </Box>
+    );
+    } else return(
+      <Box>  
+        <TitleBox>
+          <Title>¿Cuánto conocés?</Title>
+          <Triangle />
+        </TitleBox>
+        <SubBox>
+          <ReactLoading type='spin' color='#067995' height='5%' width='5%' />
+        </SubBox>
+      </Box>
+    )
 }
 
 }
