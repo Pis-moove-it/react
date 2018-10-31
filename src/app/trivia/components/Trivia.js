39007f7b4cc87a9c77b3f1ec311f1b62ffeb1e5e
Cecilia Pirotto <maria.cecilia.pirotto@fing.edu.uy>
2018-10-31 17:34:03 -0300

    Changed assignment suggestted by Lint

--- src/app/trivia/components/Trivia.js
+++ src/app/trivia/components/Trivia.js
@@ -1,89 +1,86 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import ReactLoading from 'react-loading';
 import {
   Box, Title, SubBox, Option, Question, Triangle, TitleBox, BoxOption,
 } from '../styles/trivia';
-import axios from 'axios';
-import ReactLoading from 'react-loading';
 
 class Trivia extends Component {
-
-  
   constructor(props) {
     super(props);
     this.state = {
       questions: [],
       correct: 0,
-      loading: true
+      loading: true,
     };
-
   }
 
   componentDidMount() {
-    this.getQuestions()
+    this.getQuestions();
   }
 
   getQuestions() {
-    this.setState({loading : true})
+    this.setState({ loading: true });
     axios.get('http://34.213.11.120/questions')
-      .then( ({data}) => {
+      .then(({ data }) => {
         this.setState(
-          { questions : data,
-          loading: false }
+          {
+            questions: data,
+            loading: false,
+          },
 
         );
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
         if (error.response) { // If a response has been received from the server
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
         }
-        });
+      });
   }
 
 
-
-
-render(){
-  const actualQ = this.state.questions.pop()
-  const {loading} = this.state;
-  if(!loading ){ 
-    return (
-      <Box>  
+  render() {
+    const { questions } = this.state;
+    const actualQ = questions.pop();
+    const { loading } = this.state;
+    if (!loading) {
+      return (
+        <Box>
+          <TitleBox>
+            <Title>¿Cuánto conocés?</Title>
+            <Triangle />
+          </TitleBox>
+          <SubBox>
+            <Question>{actualQ.question}</Question>
+            <BoxOption>
+              <Option>{actualQ.option_a}</Option>
+            </BoxOption>
+            <BoxOption>
+              <Option>{actualQ.option_b}</Option>
+            </BoxOption>
+            <BoxOption>
+              <Option>{actualQ.option_c}</Option>
+            </BoxOption>
+            <BoxOption>
+              <Option>{actualQ.option_d}</Option>
+            </BoxOption>
+          </SubBox>
+        </Box>
+      );
+    } return (
+      <Box>
         <TitleBox>
           <Title>¿Cuánto conocés?</Title>
           <Triangle />
         </TitleBox>
         <SubBox>
-          <Question>{actualQ.question}</Question>
-          <BoxOption>
-            <Option>{actualQ.option_a}</Option>
-          </BoxOption>    
-          <BoxOption>
-            <Option>{actualQ.option_b}</Option>
-          </BoxOption>   
-          <BoxOption>
-            <Option>{actualQ.option_c}</Option>
-          </BoxOption> 
-          <BoxOption>
-            <Option>{actualQ.option_d}</Option>
-          </BoxOption> 
+          <ReactLoading type="spin" color="#067995" height="5%" width="5%" />
         </SubBox>
       </Box>
     );
-    } else return(
-      <Box>  
-        <TitleBox>
-          <Title>¿Cuánto conocés?</Title>
-          <Triangle />
-        </TitleBox>
-        <SubBox>
-          <ReactLoading type='spin' color='#067995' height='5%' width='5%' />
-        </SubBox>
-      </Box>
-    )
-}
-
+  }
 }
 export default Trivia;
