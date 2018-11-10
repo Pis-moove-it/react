86c561eee35935c18887b011455a88b32ca28883
alexisartus96 <alexisartus@gmail.com>
2018-11-10 12:16:44 -0300

    final floating menu

--- src/app/map/styles/MapViewComponent.js
+++ src/app/map/styles/MapViewComponent.js
@@ -4,14 +4,16 @@ const MapView = styled.div`
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   box-sizing: border-box;
+  width: 100vw;
   z-index: 0;
   flex: 1;
   height: calc(100vh - 55px); /* all vertical height minus header */
   display: flex;
   flex-direction: row;
-
+  position: relative;
+  align-items: center;
   @media (max-width: 425px) {
-    flex-direction: column;
+    flex-direction: column-reverse;
   }
 `;
 
