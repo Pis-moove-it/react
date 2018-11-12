6f2444e314a83ec433cc8bb8f7ef823c5c733dde
alexisartus96 <alexisartus@gmail.com>
2018-11-11 23:05:58 -0300

    fix commented include and map width

--- src/app/map/styles/MapViewComponent.js
+++ src/app/map/styles/MapViewComponent.js
@@ -4,7 +4,7 @@ const MapView = styled.div`
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   box-sizing: border-box;
-  width: 100vw;
+  width: 100%;
   z-index: 0;
   flex: 1;
   height: calc(100vh - 55px); /* all vertical height minus header */
